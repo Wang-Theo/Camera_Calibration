25e1050ef432a0c6b38fac014e2f20bb78fb6337
renjie <renjiewang31@gmail.com>
2023-06-09 23:11:43 +0800

    first commit

---
+++ include/cam_cali/cam_pulish.hpp
@@ -0,0 +1,35 @@
+#ifndef CAM_SUBSCRIBE
+#define CAM_SUBSCRIBE
+
+#include "ros/ros.h"
+#include "sensor_msgs/CameraInfo.h"
+#include "sensor_msgs/Image.h"
+
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/objdetect.hpp>
+
+#include "ceres/ceres.h"
+#include "ceres/rotation.h"
+#include "ceres/covariance.h"
+
+#include <iostream>
+
+namespace calibration {
+class CameraNode{
+    public:
+        CameraNode(cv::VideoCapture cap);
+
+        void CamPublisher(ros::NodeHandle camera_node);
+        void ImageProcess(cv::Mat frame);
+
+        ros::NodeHandle camera_node;
+        cv::VideoCapture cap_;
+        std::string img_topic="camera/image";
+};
+} // namespace calibration
+
+#endif /* CAM_SUBSCRIBE */
---
+++ test/test_cam_publish.cpp
@@ -0,0 +1,22 @@
+#include "ros/ros.h"
+#include "cam_cali/cam_pulish.hpp"
+
+using namespace calibration;
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "img_publisher");
+
+    cv::VideoCapture cap;
+    int device_id=0;
+    if(argc>1)
+        device_id=argv[1][0]-'0';
+    int api_id=cv::CAP_ANY;
+    cap.open(device_id+api_id);
+    if(!cap.isOpened()){
+        std::cerr<<"ERROR! Unable to open camera"<<std::endl;
+        return -1;
+    }
+
+    CameraNode cam(cap);
+    return 0;
+}
