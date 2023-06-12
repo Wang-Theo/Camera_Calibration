7bcafa882102b14d9c4d3198e2738198daff3e56
renjie <renjiewang31@gmail.com>
2023-06-12 11:46:36 +0800

    bugfix: calibrate simultaneous

--- src/cam_node.cpp
+++ src/cam_node.cpp
@@ -16,18 +16,19 @@ void CameraNode::OpencvCalibrateCameraIntrinsics(std::queue<cv::Mat> images){
     std::vector<cv::Point2f> corners;
 
     // 2. Extracte corner points
-    cv::Mat image, gray;
+    cv::Mat image_, gray;
     bool found;
     for (size_t i = 0; i < img_num; i++)
-    {
-        image = images.front();
-        cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+    {   
+        image_ = images.front().clone();
+        images.pop();
+        cv::cvtColor(image_, gray, cv::COLOR_BGR2GRAY);
 
-        found = cv::findChessboardCorners(image, boardSize, corners, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK);
+        found = cv::findChessboardCorners(image_, boardSize, corners, cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE + cv::CALIB_CB_FAST_CHECK);
         if (found)
         {
             cv::cornerSubPix(gray, corners, cv::Size(11, 11), cv::Size(-1, -1), cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 30, 0.1));
-            cv::drawChessboardCorners(image, boardSize, corners, found);
+            cv::drawChessboardCorners(image_, boardSize, corners, found);
 
             std::vector<cv::Point3f> objectCorners;
             for (int j = 0; j < boardHeight; j++)
@@ -40,16 +41,15 @@ void CameraNode::OpencvCalibrateCameraIntrinsics(std::queue<cv::Mat> images){
             objectPoints.push_back(objectCorners);
             imagePoints.push_back(corners);
         }
-        images.pop();
     }
 
     // 3. Calibrate camera
     cv::Mat cameraMatrix, distCoeffs;
     std::vector<cv::Mat> rvecs, tvecs;
-    if(objectPoints.size()!=0){
-        cv::calibrateCamera(objectPoints, imagePoints, image.size(), cameraMatrix, distCoeffs, rvecs, tvecs, cv::CALIB_FIX_K3);
+    if(imagePoints.size()!=0){
+        cv::calibrateCamera(objectPoints, imagePoints, image_.size(), cameraMatrix, distCoeffs, rvecs, tvecs, cv::CALIB_FIX_K3);
 
-        cv::imwrite("/home/renjie/catkin_ws/src/Camera_Calibration/image/calibrated_image.png",image);
+        cv::imwrite("/home/renjie/catkin_ws/src/Camera_Calibration/image/calibrated_image.png",image_);
 
         std::cout << "Camera matrix:" << std::endl << cameraMatrix << std::endl;
         std::cout << "Distortion coefficients:" << std::endl << distCoeffs << std::endl;
@@ -61,17 +61,19 @@ void CameraNode::OpencvCalibrateCameraIntrinsics(std::queue<cv::Mat> images){
 
 void CameraNode::ImageCallback(const sensor_msgs::ImageConstPtr& msg){
     std::cout<<"Start calibration ! (image number: "<< img_num << ")" <<std::endl;
-    cv::Mat images_subcribe = cv_bridge::toCvShare(msg, "bgr8")->image;
+    cv::Mat images_subcribe = cv_bridge::toCvShare(msg, "rgb8")->image;
     int img_id = 1;
+    bool flag = false;
     while(!images_subcribe.empty()){
         img_saved.push(images_subcribe);
-        if (img_id>img_num) {
+        if (flag == true) {
             img_saved.pop();
             CameraNode::OpencvCalibrateCameraIntrinsics(img_saved);
         }
 
         if(img_id == img_num){
             image_exist_flag = 1;
+            flag = true;
         }
         img_id++;
     }
